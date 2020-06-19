586419879076607e00a9f440985c92f8a8c57162
fujisho1 <67129581+fujisho1@users.noreply.github.com>
2020-06-19 11:03:30 +0900

    Fix miss spell

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-vurtualenv rsvpapp --system-site-packages -v
+virtualenv rsvpapp --system-site-packages -v
 source rsvpapp/bin/activate
 pip install -r requirements.txt
 pytest tests/test_rsvpapp.py
